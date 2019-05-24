d4495bc846fe7c8e0a02c980ab98bdd3bee0e709
Nathan Szelag <nszelag@live.com>
2019-05-23 21:32:07 -0400

    added render results and reset search

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ $(document).ready(function() {
     const userSearchValue = $("#user").val();
     fetch(`https://api.github.com/users/${userSearchValue}/repos`)
       .then(response => response.json())
-      .then(response => console.log(response));
+      .then(response => renderResults(response));
+  }
+
+  function renderResults(response) {
+      console.log(response[0]['name']);
+      console.log(response[0]["html_url"]);
+      for(let i = 0; i < response.length; i++){
+      $('#results').append(`<p><b>${response[i]['name']}:</b> <a href="${response[i]["html_url"]}">${response[i]["html_url"]}</a>`)
+      }
+  }
+
+  function resetSearch(){
+      $('#results').html('');
   }
 
   function watchForm() {
     $("#userSearch").on("submit", function(event) {
       event.preventDefault();
-
+        resetSearch();
       searchGithub(user);
     });
   }
